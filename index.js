b9a6b7bd291553f25a6d123b16af5948c845a76c
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-06 05:14:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name){
-    return cats.push(name)
+function destructivelyAppendCat (name) {
+  cats.push(name);
 }
 
-function destructivelyPrependCat(name){
-    return cats.unshift(name)
+function destructivelyPrependCat (name) {
+  cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(){
-    return cats.pop()
+function destructivelyRemoveLastCat () {
+  cats.pop();
 }
 
-function destructivelyRemoveFirstCat(){
-    return cats.shift()
+function destructivelyRemoveFirstCat () {
+  cats.shift();
 }
 
-function appendCat(name){
-    return [...cats, name]
+function appendCat (name) {
+  return [...cats, name];
 }
 
-function prependCat(name){
-    return [name, ...cats]
+function prependCat (name) {
+  return [name, ...cats];
 }
 
-function removeLastCat(){
-    return cats.slice(0,cats.length-1)
+function removeFirstCat () {
+  return cats.slice(1);
 }
 
-function removeFirstCat(){
-    return cats.slice(1)
+function removeLastCat () {
+  return cats.slice(0, cats.length - 1);
 }
